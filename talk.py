73f453e2f23821766d439721ad6d243fce634ea9
MilesNaismith <truskov@inbox.ru>
2018-09-11 14:01:31 +0300

    talk.py programm with exception

--- talk.py
+++ talk.py
@@ -1,7 +1,11 @@
 def ask_user():
     vocabulary = {'Привет':'Привет!!', 'Как дела?':'Хорошо', 'Где бы ты хотел работать?': 'В Aperture Science', 'Что делаешь?':'Программирую', 'Кем бы ты хотел стать?':'ОБЧР(Огромным Боевым Человекоподобным Роботом)'}
     while True:
-        frase = input('Человек: ')
+        try:
+            frase = input('Человек: ')
+        except(KeyboardInterrupt):
+            print('Пока!')
+            break    
         if frase in vocabulary:
             print('Программа: ' + vocabulary[frase])
         else:
