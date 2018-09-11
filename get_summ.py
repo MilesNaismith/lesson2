82e315ae2e13fb982512b34fb95486dc5c5b73ed
MilesNaismith <truskov@inbox.ru>
2018-09-11 14:06:54 +0300

    get_summ.py programm

---
+++ get_summ.py
@@ -0,0 +1,9 @@
+def get_summ(num_one, num_two):
+    try:
+        num_one = int(num_one)
+        num_two = int(num_two)
+    except(ValueError):
+        return 'Требуется указать числа'    
+    return num_one + num_two
+
+print(get_summ('qwe','-564'))    
