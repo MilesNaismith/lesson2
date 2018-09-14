63bc7a6653593e85f4101fa1a8694c1602f33f4d
MilesNaismith <truskov@inbox.ru>
2018-09-14 10:59:25 +0300

    talk.py:try-block changed

--- talk.py
+++ talk.py
@@ -4,15 +4,14 @@ def ask_user():
                   'Где бы ты хотел работать?': 'В Aperture Science', 
                   'Что делаешь?':'Программирую', 
                   'Кем бы ты хотел стать?':'ОБЧР(Огромным Боевым Человекоподобным Роботом)',
-                  }
-    while True:
-        try:
+                 }
+    try:              
+        while True:
             frase = input('Человек: ')
-        except(KeyboardInterrupt):
-            print('Пока!')
-            break    
-        if frase in vocabulary:
-            print('Программа: ' + vocabulary[frase])
-        else:
-            print('Программа: Я тебя не понимаю, попробуй сформулировать свой вопрос иначе')   
+            if frase in vocabulary:
+                print('Программа: ' + vocabulary[frase])
+            else:
+                print('Программа: Я тебя не понимаю, попробуй сформулировать свой вопрос иначе')   
+    except(KeyboardInterrupt):
+        print('Пока!')
 ask_user()
