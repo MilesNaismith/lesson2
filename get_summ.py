d191feaa6fed87dbb7a4635db903feb55bd846ac
MilesNaismith <truskov@inbox.ru>
2018-09-14 11:05:26 +0300

    get_summ.py: try-block changed

--- get_summ.py
+++ get_summ.py
@@ -2,7 +2,7 @@ def get_summ(num_one, num_two):
     try:
         num_one = int(num_one)
         num_two = int(num_two)
-    except(ValueError):
+    except ValueError:
         return 'Требуется указать числа'    
     return num_one + num_two
 
