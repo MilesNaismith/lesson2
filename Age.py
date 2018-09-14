a2c3dfda949e019ccc0f571a1dc0eb73b02f8af7
MilesNaismith <truskov@inbox.ru>
2018-09-14 09:51:40 +0300

    Age.py: changed 'elif' conditions

--- Age.py
+++ Age.py
@@ -3,13 +3,14 @@ age = input('Введите свой возраст, пожалуйста: ')
 def what_is_your_duty(age):
     try:
         age = int(age)
-    except(ValueError):
+    except ValueError:
         return 'Требуется ввести число'
+      
     if age < 7:
         return 'Тебе следует учиться в детском саду'
-    elif age >= 7 and age < 17:
+    elif age age < 17:
         return 'Тебе следует учиться в школе'
-    elif age >= 17 and age < 23:
+    elif age < 23:
         return 'Тебе следует учиться в ВУЗе'
     else:
         return 'Тебе следует работать' 
