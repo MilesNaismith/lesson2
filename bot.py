2f9aa26c809eab7171b92f9ba9da3157551bbef6
Truskov Artur <truskov@inbox.ru>
2018-09-15 16:49:48 +0300

    bot.py: calc, word count functions

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 from tokentoken import API_TOKEN
-import ephem
+#import ephem
 from datetime import datetime, date, time
+import telegram
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -31,12 +32,70 @@ def planet_user(bot,update):
     print(text)
     update.message.reply_text(text)
 
+def word_count(bot, update):
+    user_text = update.message.text.split()
+    if len(user_text) == 1:
+        text = 'Требуется ввести фразу'
+    elif len(user_text) == 2 and user_text[1] =='""':
+        text = 'Вы просто написали кавычки'
+    elif len(user_text) == 2 and user_text[1] =='"':
+        text = 'Вы просто написали кавычку'            
+    elif user_text[1].startswith('"') and user_text[-1].endswith('"'):
+        text = str(len(user_text) - 1)
+        if text.endswith('1'):
+            text = text + ' слово'
+        elif text.endswith('2') or text.endswith('3'):
+            text = text + ' слова'
+        else:
+            text = text + ' слов'        
+    else:
+        text = 'Где кавычки?'        
+    print(text)
+    update.message.reply_text(text)   
+
+def bot_calc(bot, update):
+    user_text = update.message.text.split()
+    if len(user_text) == 1:
+        answer = 'Что будем считать? Отсутствует выражение!'
+        print(answer)
+        update.message.reply_text(answer)
+    elif len(user_text) > 2:
+        answer = 'Неверный формат, уберите пробелы в выражении'
+        print(answer)
+        update.message.reply_text(answer)
+    else:
+        text = user_text[1]
+
+    if '+' in text:
+        x = int(text[:text.index('+')])
+        y = int(text[text.index('+') + 1:text.index('=')])
+        answer = x + y
+    elif '-'in text:
+        x = int(text[:text.index('-')])
+        y = int(text[text.index('-') + 1:text.index('=')])
+        answer = x - y
+    elif '*' in text:
+        x = int(text[:text.index('*')])
+        y = int(text[text.index('*') + 1:text.index('=')])
+        answer = x * y
+    elif '/' in text:
+        x = int(text[:text.index('/')])
+        y = int(text[text.index('/') + 1:text.index('=')])
+        if y == 0:
+            answer = 'на ноль делить нельзя'
+        else:
+            answer = x / y       
+    print(answer)
+    update.message.reply_text(answer)            
 
 def main():
     mybot = Updater(API_TOKEN, request_kwargs=PROXY)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(CommandHandler('planet', planet_user))
+    dp.add_handler(CommandHandler('wordcount', word_count))
+    dp.add_handler(CommandHandler('calc', bot_calc))
+    dp.add_handler(CommandHandler('keyboard', calc_keyboard))
     #Начало цикла
     mybot.start_polling()
     mybot.idle()
