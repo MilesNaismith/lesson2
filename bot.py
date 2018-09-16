a75feb996eea7476c451d0cccf4fb2644b0319ac
Truskov Artur <truskov@inbox.ru>
2018-09-16 17:32:57 +0300

    bot.py calc with keybord aded(division)

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
-from tokentoken import API_TOKEN
-#import ephem
+from tokentoken import API_TOKEN, id_chat
+import ephem
 from datetime import datetime, date, time
 import telegram
 
@@ -83,9 +83,9 @@ def bot_calc(bot, update):
         x = int(text[:text.index('*')])
         y = int(text[text.index('*') + 1:text.index('=')])
         answer = x * y
-    elif '/' in text:
-        x = int(text[:text.index('/')])
-        y = int(text[text.index('/') + 1:text.index('=')])
+    elif ':' in text:
+        x = int(text[:text.index(':')])
+        y = int(text[text.index(':') + 1:text.index('=')])
         if y == 0:
             answer = 'на ноль делить нельзя'
         else:
@@ -97,20 +97,20 @@ def calc_keyboard(bot, update):
     global a
     custom_keyboard = [['1', '2', '3', '4', '5'], 
                        ['5', '6', '7', '8', '9'],
-                       ['0', '+', '-', '*', '/', '=']]
+                       ['0', '+', '-', '*', ':', '=']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
     
     a = a + update.message.text
     if a.endswith('='):
         bot_calc(bot, update)
         a = ''    
-    bot.send_message(chat_id= 450364038,
+    bot.send_message(chat_id= id_chat,
                      text = a,
                      reply_markup=reply_markup)
   #  update.message.reply_text(', '.join(a))                  
 def keyboard_off(bot, update):
     reply_markup = telegram.ReplyKeyboardRemove()
-    bot.send_message(chat_id=450364038, text="I'm back.", reply_markup=reply_markup)               
+    bot.send_message(chat_id=id_chat, text="I'm back.", reply_markup=reply_markup)               
     
 
 def main():
